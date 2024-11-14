d88aab9f7705ae7703ab9b5ac36954c3e1b06e85
Metronox <samuksantos1235@gmail.com>
2024-11-14 20:38:53 -0300

    teste 18

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-
-// Define a porta para a aplicação. Usa a variável de ambiente PORT, ou 8104 como padrão.
-const PORT = process.env.PORT || 8104;
+const port = 8104;
 
 app.get("/", (req, res) => {
-  res.send("Shadow o ouriço"); // Resposta ao acessar a rota principal
+  res.send("Shadow o ouriço");
 });
 
-// Inicia o servidor na porta definida e exibe uma mensagem no console.
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
 });
